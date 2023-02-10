d0d816f4ff2dfce226704ee343b005c8eb216fb0
D-w-nJ <jdu2121@ajou.ac.kr>
2023-02-10 23:42:29 +0900

    Refactor : about waste repository

---
+++ backend/src/main/java/com/backend/waste/repository/WasteRepositoryImpl.java
@@ -0,0 +1,45 @@
+package com.backend.waste.repository;
+
+import com.backend.member.domain.Member;
+import com.backend.waste.domain.Waste;
+import com.backend.waste.exception.WasteNotFoundException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@RequiredArgsConstructor
+@Repository
+public class WasteRepositoryImpl implements WasteRepository {
+
+    private final WasteJpaRepository wasteJpaRepository;
+
+    @Override
+    public Waste save(Waste waste) { return wasteJpaRepository.save(waste); }
+
+    @Override
+    public Optional<Waste> findByImageName(String imageName) {
+        return Optional.empty();
+    }
+
+    @Override
+    public Optional<Waste> findById(Long wasteIdx) {
+        return Optional.empty();
+    }
+
+    @Override
+    public Waste getByImageName(String imageName) {
+        return wasteJpaRepository.findByImageName(imageName).orElseThrow(WasteNotFoundException::new);
+    }
+
+    @Override
+    public List<Waste> getWasteList(Member member) {
+        return wasteJpaRepository.findAllByMemberOrderByEnrolledDateDesc(member);
+    }
+
+    @Override
+    public Waste getById(Long wasteIdx) {
+        return wasteJpaRepository.findById(wasteIdx).orElseThrow(WasteNotFoundException::new);
+    }
+}
--- backend/src/main/java/com/backend/waste/repository/WasteRepositoryInpl.java
+++
@@ -1,4 +0,0 @@
-package com.backend.waste.repository;
-
-public class WasteRepositoryInpl {
-}
