83ffa2a8020bf02490c9b18909b9cabc0e145d05
Mango-Juice <wjsalsrb5@naver.com>
2023-02-23 20:39:15 +0900

    feat: 주의 사항 추가

--- src/components/atoms/Notice.js
+++ src/components/atoms/Notice.js
@@ -6,9 +6,9 @@ export default Notice = ({ ...props }) => {
     return (
         <Container {...props}>
             <Title>📌 주의 사항</Title>
-            <Content>1. 이것 저것을 주의하세요.</Content>
-            <Content>2. 이것 저것을 주의하세요.</Content>
-            <Content>3. 이것 저것을 주의하세요.</Content>
+            <Content>1. 이미지 등록시 품목 확인을 위해 쓰레기 전체가 명확하게 찍힌 이미지를 선택해주세요.</Content>
+            <Content>2. 쓰레기는 등록 후 수거자가 매칭되어야 수거가 진행되며, 최대 3~4일이 소요될 수 있습니다. 자세한 내용은 예약 현황을 참고해주세요.</Content>
+            <Content>3. 등록한 이미지와 실제 쓰레기가 다른 경우 현장에서 수거가 거부될 수 있습니다.</Content>
         </Container>
     );
 };
--- src/navigations/Stack.js
+++ src/navigations/Stack.js
@@ -30,7 +30,7 @@ const StackNavigation = () => {
     return (
         <>
             <Stack.Navigator initialRouteName="Home" screenOptions={{ headerBackTitleVisible: false }}>
-                <Stack.Screen name="Main" component={Main} options={{ title: '(서비스 이름)' }} />
+                <Stack.Screen name="Main" component={Main} options={{ title: '쓰레기 수거 서비스' }} />
                 <Stack.Screen name="Login" component={Login} options={{ title: '로그인' }} />
                 <Stack.Screen name="Register" component={Register} options={{ title: '쓰레기 등록' }} />
                 <Stack.Screen name="Reservation" component={Reservation} options={{ title: '예약 현황' }} />
