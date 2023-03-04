8fbdc2ce7a698936d24982b97fb3e3a7b4fcf823
D-w-nJ <jdu2121@ajou.ac.kr>
2023-03-04 21:50:36 +0900

    Test : domain-waste

--- backend/src/test/java/com/backend/util/DomainTest.java
+++ backend/src/test/java/com/backend/util/DomainTest.java
@@ -1,7 +1,9 @@
 package com.backend.util;
 
+import com.backend.collector.domain.Collector;
 import com.backend.member.domain.Member;
 import com.backend.member.domain.MemberSession;
+import com.backend.waste.domain.Waste;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.crypto.scrypt.SCryptPasswordEncoder;
 
@@ -31,5 +33,21 @@ public class DomainTest {
                 .build();
 
         return memberSession;
+    }
+
+    protected Waste getWaste(){
+        Waste waste = Waste.createWaste(getMember(),"wasteImage");
+        waste.update("냉장고-300L이상",4000);
+
+        return waste;
+    }
+
+    protected Collector getCollector(){
+        Collector collector = Collector.builder()
+                .phoneNumber("010-0000-0000")
+                .name("독산동 의자 컬렉터")
+                .build();
+
+        return collector;
     }
 }
